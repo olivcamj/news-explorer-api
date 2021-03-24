59fe2a0f25a3286fe1af5c6beb9eea873f55556c
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-03-23 20:01:05 -0400

    refactor: rm temp authorization solution

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/user');
 const articleRouter = require('./routes/article');
 const { createUser, login } = require('./controllers/user');
+const NotFoundError = require('./errors/not-found-err');
+const auth = require('./middleware/auth.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,20 +17,29 @@ mongoose.connect('mongodb://localhost:27017/news', {
 });
 
 app.use(express.json());
-// Temporary authorization solution
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6052500d7f0774385b8dfa8a', // _id of the test user created in db
-  };
 
-  next();
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
 });
 
 app.post('/signup', createUser);
 app.post('/signin', login);
 
-app.use('/users', userRouter);
-app.use('/articles', articleRouter);
+app.use('/users', auth, userRouter);
+app.use('/articles', auth, articleRouter);
+app.use(() => {
+  throw new NotFoundError('Page not found');
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'An error occurred on the server' : message,
+  });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
