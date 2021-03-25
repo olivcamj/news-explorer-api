17d91662d871bf9e64a3cabb948e641fd3a48d1b
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-03-25 11:09:17 -0400

    feat: implement data validation

--- routes/article.js
+++ routes/article.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const articleRouter = express.Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getArticles,
@@ -9,10 +10,32 @@ const {
 } = require('../controllers/article.js');
 
 // returns information about the logged-in user (email and name)
-articleRouter.get('/', getArticles);
+articleRouter.get('/', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
+  }).options({ allowUnknown: true }),
+}), getArticles);
 
-articleRouter.post('/', createArticle);
+articleRouter.post('/', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
+  }).options({ allowUnknown: true }),
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().pattern(/(https?:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(:?\d*)\/?([a-z_/0-9\-#.]*)\??([a-z_/0-9\-#=&]*)/),
+    image: Joi.string().required(),
+  }),
+}), createArticle);
 
-articleRouter.delete('/:articleId', deleteArticle);
+articleRouter.delete('/:articleId', celebrate({
+  headers: Joi.object().keys({
+  }).unknown(true),
+  params: Joi.object().keys({
+    articleId: Joi.string().hex().length(24),
+  }),
+}), deleteArticle);
 
 module.exports = articleRouter;
