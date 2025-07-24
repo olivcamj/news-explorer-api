71b1edc5562245afa93af9d0ec3e16fe876df071
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-07-24 15:49:09 -0400

    refactor: clean up import

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+  const router = express.Router();
 const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./user');
 const articleRouter = require('./article');
