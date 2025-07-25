1517b57f48232ace55f579b2145e33cd6b764b9e
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-07-25 00:47:52 -0400

    fix: increase number of requests after testing

--- middleware/rate-limit.js
+++ middleware/rate-limit.js
@@ -3,7 +3,7 @@ const { reachedLimit } = require('../utils/constants');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // in 15 minutes
-  max: 1, // a maximum of 100 requests from one IP
+  max: 100, // a maximum of 100 requests from one IP
   standardHeaders: 'draft-8',
   legacyHeaders: false,
   handler: async (req, res) => {
